06435894193203a9118a563ddc603c8be20dea18
KRDevs <113543958+KRDevs@users.noreply.github.com>
2023-03-14 01:11:06 +0500

    new blog creating

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView,BlogCreateView
 
 urlpatterns = [
+    path('post/new/',BlogCreateView.as_view(),name='post_new'),
     path('', BlogListView.as_view(), name='main'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 # Create your views here.
@@ -11,3 +12,7 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title','author','body']
